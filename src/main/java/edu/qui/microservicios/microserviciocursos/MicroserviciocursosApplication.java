b3b23cfdc7c034af74087467b8a569f19387dc8c
Enrique Sancho <quiquesanchocaballer@gmail.com>
2021-10-06 12:00:23 +0200

    Añadimos configuración de Eureka

--- src/main/java/edu/qui/microservicios/microserviciocursos/MicroserviciocursosApplication.java
+++ src/main/java/edu/qui/microservicios/microserviciocursos/MicroserviciocursosApplication.java
@@ -3,9 +3,11 @@ package edu.qui.microservicios.microserviciocursos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EntityScan("edu.qui.microservicios")
+@EnableEurekaClient
 public class MicroserviciocursosApplication {
 
 	public static void main(String[] args) {
